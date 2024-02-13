712a561bbc5747a39bb4a9866009979d3cad73ae
olegggwp <boolvargosha@yandex.ru>
2024-02-13 23:51:53 +0300

    sss

--- run_bunch.sh
+++ run_bunch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 script="$1"
-for ((N=1; N<=2; N++))
+for ((N=1; N<=20; N++))
 do
     echo "$N:" >> "$script".time
-    for ((i=1; i<=2; i++))
+    for ((i=1; i<=10; i++))
     do
         { \time -f "%e" bash "$script" "$N" ; } 2>> "$script".time
     done
