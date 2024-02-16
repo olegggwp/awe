a5cc0ef2603ad511e95d27e37e00dcbc406d6ece
olegggwp <boolvargosha@yandex.ru>
2024-02-16 12:18:53 +0300

    glkrgj

--- test_RunBunch_script.sh
+++ test_RunBunch_script.sh
@@ -2,11 +2,11 @@
 
 script="$1"
 echo "" > "$script".time
-for ((N=1; N<=20; N++))
+for ((N=1; N<=2; N++))
 do
     ./gen
     echo "$N:" >> "$script".time
-    for ((i=1; i<=10; i++))
+    for ((i=1; i<=5; i++))
     do
         { \time -f "%e" bash "$script" "$N" ; } 2>> "$script".time
     done
